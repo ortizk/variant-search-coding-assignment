11fa7696472ea6a86de147850e7fc892ae46a0c9
ortizk <ortizk@live.com>
2018-11-28 10:08:52 -0800

    filled empty cells with nan

--- invitae/find_variants.py
+++ invitae/find_variants.py
@@ -1,9 +1,14 @@
 from pandas_variants import *
 
+# DELETE
+data = read_data()
+
 def find_gene(gene, data):
 	results = []
 	for d in data.values:
-		if d[0] == gene:
-			results.append(d[1])
-	return results
+		if d[0] == gene.upper():
+			results.append(d[0])
+	print(results)
 
+# DELETE
+find_gene("braf", data)
--- invitae/suggest_gene.py
+++ invitae/suggest_gene.py
@@ -1,16 +1,41 @@
 from pandas_variants import *
 
-
 data = read_data()
 
-letters = "b"
+letters = "B"
 
 def auto_complete(letters, data):
 	results = []
 	length = len(letters)
+	print(data.values[0])
+	print('this', isinstance(data.values[7][0], float))
+	print(letters)
+	# converting empty cells to Nan 
 	for l in data.values:
-		if l[: length][0] == letters:
-		  results.append(l)
-	print(results)
+		if isinstance(l[0], float):
+			l[0] = float('nan')
+			# works until here
+	data.fillna(method='ffill')
+
+
+
+
+
+	# for l in data.values:
+		# data.apply(data.to_numeric, args=('coerce',))
+		# if isinstance(l[0], float):
+			# l[0].convert_objects(convert_numeric=True)
+			# l[0] = np.NaN
+			# data.fillna(method='ffill')
+		# print(l[0])
+		# print (l[0][:2])
+		# if l[0]:
+			# print(str(l[0][:2]))
+		# 	print(l[0])
+		# if str(l[0][: length]) == letters:
+		  # results.append(l)
+	# print(results)
+		
+
 
 auto_complete(letters, data)	
