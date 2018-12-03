a5ecb2c119375e6262263889cc67c447e6680159
ortizk <ortizk@live.com>
2018-12-02 19:11:54 -0800

    deploy

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,3 @@
-module.exports.SERVER_URL = "http://127.0.0.1:8000" 
+// module.exports.SERVER_URL = "http://127.0.0.1:8000" 
+
+module.exports.SERVER_URL = "https://mysterious-dawn-16650.herokuapp.com"
