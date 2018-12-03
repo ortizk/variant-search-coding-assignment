61c9a2753cb6c4fdda989c592734dfc076b15a2d
ortizk <ortizk@live.com>
2018-12-03 11:46:58 -0800

    Revert "db"
    This reverts commit c7fdf1d8f0e024a45630ca99cf6c08d9357383a9.

--- server/settings.py
+++ server/settings.py
@@ -80,12 +80,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
