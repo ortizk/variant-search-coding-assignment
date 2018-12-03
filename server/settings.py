d947a68728ff0b7b2d8e361e4569fa54cad5ae63
ortizk <ortizk@live.com>
2018-12-03 11:46:18 -0800

    Revert "default to db"
    This reverts commit bbb025a58762416bdf5548814900260c4087a92f.

--- server/settings.py
+++ server/settings.py
@@ -81,9 +81,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
-    'default':{}
-}
+DATABASES = {}
 
 
 # Password validation
