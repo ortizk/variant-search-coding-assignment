58ea5b50f6ee03e3dd84e5240c133ba82dbf707c
ortizk <ortizk@live.com>
2018-12-02 21:28:00 -0800

    engines

--- server/settings.py
+++ server/settings.py
@@ -82,7 +82,9 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
-    'default':{}
+    'default':{
+        'ENGINE': ''
+    }
 }
 
 
